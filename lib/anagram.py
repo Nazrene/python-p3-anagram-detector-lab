7749250e623a6bedc033cc3363a35930cdef0aef
Naz <mercy.kamau@student.moringaschool.com>
2024-09-15 04:53:07 +0300

    Anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        anagrams = []
+
+        sorted_word = sorted(self.word)
+
+        for candidate in word_list:
+            if sorted(candidate.lower()) == sorted_word:
+                anagrams.append(candidate)
+
+        return anagrams
